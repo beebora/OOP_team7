1ecf8b32956c49e2dece5b30f23e88d4ec7d9106
moralkingMB <hdh313158@gmail.com>
2012-12-14 03:14:38 +0900

    돌아와요윤승배

--- MultiServer/src/controller/dbms.java
+++ MultiServer/src/controller/dbms.java
@@ -8,15 +8,64 @@ import java.sql.*;
 public class dbms {
 	String SQL;
 	Statement stmt;
-	
+	ResultSet rs;
+	public class member
+	{
+		String id, name, pass, parent;
+		boolean logon, is_dept;
+		
+	}
 	private void init_class()
 	{
 		SQL = null;
 		stmt = null;
-	}
-	public void create_table()
-	{
 		
+	}
+	public boolean create_table()
+	{	
+		init_class();
+
+		SQL="create table member(id varchar(20),name varchar(20), pass varchar(20),  parent varchar(20), logon bool, is_dept bool, primary key(id), foregin key(parent) references member) ENGINE=MyISAM DEFAULT CHARSET=utf8	";
+			
+		try
+		{
+			stmt.executeUpdate(SQL);
+
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+
+		
+	}
+	public ArrayList<member> getList()
+	{
+		init_class();
+
+		ArrayList<member> mem;
+		mem = new ArrayList<member>();
+		SQL="SELECT * FROM member";
+		try
+		{
+			
+			rs = stmt.executeQuery(SQL);
+			while(rs.next())
+			{
+				member me = new member();
+				me.id = rs.getString("id");
+				me.is_dept = rs.getBoolean("is_dept");
+				me.logon = rs.getBoolean("logon");
+				me.name = rs.getString("name");
+				me.parent = rs.getString("parent");
+				me.pass = rs.getString("pass");
+				mem.add(me);
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		return mem;
 	}
 	
 	public boolean join(String name, String id, String pass, String dept){
@@ -36,7 +85,7 @@ public class dbms {
 	}
 	public boolean logout(String id){
 		init_class();
-		SQL = "update term.member set ip = '0' where id = '"+id+"'";
+		SQL = "update term.member set logon = 'false' where id = '"+id+"'";
 		try
 		{
 			stmt.executeUpdate(SQL);
@@ -48,21 +97,71 @@ public class dbms {
 		}
 	}
 	public boolean login(String id, String pass, String ip){
-		init_class();
-		// 찾아보고, 있으면 t else false
+		init_class();			
+		SQL="SELECT * FROM term.member where id='"+id+"' and pass='"+pass+"'";
 		try
 		{
-			///SQL = "update term.member() set "
-			stmt.executeUpdate(SQL);
+
+			rs = stmt.executeQuery(SQL);
+			if (stmt.execute(SQL)) 
+			{
+				rs = stmt.getResultSet();
+			}
+		} 
+		catch(Exception ex) 
+		{
+			System.out.println(ex.toString());
+			return false;
+		}
 			
+		int i=0;
+		try
+		{
+			while (rs.next())
+			{
+					i++;break;
+			} 
+		}
+		catch(Exception ex)
+		{
+				System.out.println(ex.toString());
+		}
+		if(i==0)
+		{
+			return false;
+		}
+		else
+		{
+			SQL = "update term.member set logon = 'true' where id = '"+id+"'";
+			try
+			{
+				stmt.executeUpdate(SQL);
+				return true;
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+				return false;
+			}	
+		}
+	}
+		
+	public boolean getMember(String id)
+	{		
+		init_class();
+		SQL = "update term.member set logon = 'false' where id = '"+id+"'";
+		try
+		{
+			stmt.executeUpdate(SQL);
 			return true;
 		}
 		catch(Exception e){
 			e.printStackTrace();
 			return false;
 		}
+		
 	}
-	public boolean setIP(String id, String ip){
+	/*public boolean setIP(String id, String ip){
 		init_class();
 		SQL = "update term.member set ip = '"+ip+"' where id = '"+id+"'";
 		try
@@ -74,7 +173,7 @@ public class dbms {
 			e.printStackTrace();
 			return false;
 		}
-	}
+	}*/
 	
 //getmember -- dept/name
 	//input = dept id
